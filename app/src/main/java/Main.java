b5fe2864b6c0057cd634783a89e1f7c97d788ed3
ueharakosuke <ueharakosuke@ueharakosukenoMacBook-Air.local>
2023-02-03 18:05:56 +0900

    report4

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -5,10 +5,11 @@ import jp.ac.uryukyu.ie.e225708.*;
          Hero hero = new Hero("勇者", 10, 5);
          Enemy enemy = new Enemy("スライム", 6, 3);
 
-         System.out.printf("%s vs. %s\n", hero.name, enemy.name);
+         System.out.printf("%s vs. %s\n", hero, enemy);
 
          int turn = 0;
-         while( hero.dead == false && enemy.dead == false ){
+         while( hero.getDead() == false && enemy.getDead() == false ){
+            turn++;
              turn++;
              System.out.printf("%dターン目開始！\n", turn);
              hero.attack(enemy);
