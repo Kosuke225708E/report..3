44daf963b4f4aa8ac74fa808a4dd6a70af363110
ueharakosuke <ueharakosuke@ueharakosukenoMacBook-Air.local>
2023-02-08 14:08:20 +0900

    report3.step3.

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -1,21 +1,21 @@
 import jp.ac.uryukyu.ie.e225708.*;
 
  public class Main {
-     public static void main(String[] args){
-        Warrior hero = new Warrior("勇者", 10, 5);
-         Enemy enemy = new Enemy("スライム", 6, 3);
 
-         System.out.printf("%s vs. %s\n", hero, enemy);
+      public static void main(String[] args){
+           Hero hero = new Hero("勇者", 10, 5);
+           Enemy enemy = new Enemy("スライム", 6, 3);
+  
+         System.out.printf("%s vs. %s\n", hero.getName(), enemy.getName());
 
          int turn = 0;
-         while( hero.isDead() == false && enemy.isDead() == false ){
+         while( hero.Dead_s() == false && enemy.Dead_s() == false ){
             turn++;
-             turn++;
              System.out.printf("%dターン目開始！\n", turn);
-             hero.attackWithWeponSkill(enemy);
-             enemy.attack(hero);
-         }
-         System.out.println("戦闘終了");
-      }
- }
+             hero.attack(enemy);
+            enemy.attack(hero);
+        }
+        System.out.println("戦闘終了");
+    }
+}
 
--- app/src/main/java/jp/ac/uryukyu/ie/e225708/Hero.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e225708/Hero.java
@@ -8,19 +8,69 @@ package jp.ac.uryukyu.ie.e225708;
   *  boolean dead; //敵の生死状態。true=死亡。
   * Created by tnal on 2016/11/13.
   */
-  public class Hero extends LivingTing {
-    public Hero(String name, int maximumHP, int attack){
-        super(name, maximumHP, attack);
+  public class Hero  {
+   private String name;
+   private int hitPoint;
+   private int attack;
+   private boolean dead;
+
+
+
+   public String getName(){
+       return this.name;
+   }
+
+   public int getHitPoint(){
+       return this.hitPoint;
+   }
+
+   public int getAttack(){
+       return this.attack;
+   }
+
+   public boolean Dead_s(){
+       return this.dead;
+   }
+   
+   public Hero (String _name, int _maximumHP, int _attack) {
+      this.setName(_name);
+      this.setHitPoint(_maximumHP);
+      this.setAttack(_attack);
+      dead = false;
+
+      System.out.printf("%sのHPは%d。攻撃力は%dです。\n", _name, _maximumHP, _attack);
      }
 
-     @Override
+     public void setName(String _name){
+         this.name = _name;
+     }
+
+     public void setHitPoint(int _maximumHP){
+         this.hitPoint = _maximumHP;
+     }
+
+     public void setAttack(int _attack){
+         this.attack = _attack;
+     }
 
      
-     public void wounded(int damage){
-        hitPoint -= damage;
-         if( hitPoint < 0 ) {
-            dead = true;
-            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", getName());
+     public void attack(Enemy e){
+      if(this.dead == false){
+         int damage = (int)(Math.random() * this.getAttack());
+         System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", this.getName(), e.getName(), damage);
+         e.wounded(damage);
+     }
+
+ }
+
+ 
+ public void wounded(int damage){
+   this.hitPoint -= damage;
+   if( hitPoint < 0 ) {
+      this.dead = true;
+             System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", this.getName());
          }
      }
  }
+
+     
